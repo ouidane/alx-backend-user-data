5a31e60e2a81957169abe9024f3cd1568c3d7cca
ouidane <ouidanezakaria@gmail.com>
2025-05-23 00:52:55 +0100

    encrypt password

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -9,8 +9,7 @@ def hash_password(password: str) -> bytes:
     """
     password_bytes = password.encode('utf-8')
     salt = bcrypt.gensalt()
-    hashed = bcrypt.hashpw(password_bytes, salt)
-    return hashed
+    return bcrypt.hashpw(password_bytes, salt)
 
 def is_valid(hashed_password: bytes, password: str) -> bool:
     """Validates a password against a given hashed password.

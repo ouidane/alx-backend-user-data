0b1031e421d2fbae7593fafa45cf0b36bfaac4ab
ouidane <ouidanezakaria@gmail.com>
2025-05-23 00:50:34 +0100

    encrypt password

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,19 +1,11 @@
 #!/usr/bin/env python3
-"""
-Module that provides functions to hash and validate passwords using bcrypt.
+"""Module that provides functions to hash and validate passwords using bcrypt.
 """
 
 import bcrypt
 
 def hash_password(password: str) -> bytes:
-    """
-    Hashes a password with a randomly-generated salt using bcrypt.
-
-    Args:
-        password (str): The password to hash.
-
-    Returns:
-        bytes: The salted, hashed password.
+    """Hashes a password with a randomly-generated salt using bcrypt.
     """
     password_bytes = password.encode('utf-8')
     salt = bcrypt.gensalt()
@@ -21,14 +13,6 @@ def hash_password(password: str) -> bytes:
     return hashed
 
 def is_valid(hashed_password: bytes, password: str) -> bool:
-    """
-    Validates a password against a given hashed password.
-
-    Args:
-        hashed_password (bytes): The previously hashed password.
-        password (str): The plain text password to validate.
-
-    Returns:
-        bool: True if the password is valid, False otherwise.
+    """Validates a password against a given hashed password.
     """
     return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
